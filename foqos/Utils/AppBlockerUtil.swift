da00c5750e1406542859d8149c152d8a37ec983f
Ali Waseem <waseema393@gmail.com>
2025-06-02 19:56:54 -0600

    removed device monitor, way to buggy right now

--- foqos/Utils/AppBlockerUtil.swift
+++ foqos/Utils/AppBlockerUtil.swift
@@ -35,24 +35,6 @@ class AppBlockerUtil {
         }
         
         store.application.denyAppRemoval = strict
-        
-        SharedData.selection = selection
-        SharedData.strict = strict
-        SharedData.allowOnly = allowOnly
-
-        // Set up a DeviceActivitySchedule
-        let schedule = DeviceActivitySchedule(
-            intervalStart: DateComponents(hour: 0, minute: 0),
-            intervalEnd: DateComponents(hour: 23, minute: 59),
-            repeats: true
-        )
-
-        do {
-            let center = DeviceActivityCenter()
-            try center.startMonitoring(.daily, during: schedule)
-        } catch {
-            print("Error starting monitoring: \(error)")
-        }
     }
 
     func deactivateRestrictions() {
@@ -65,12 +47,5 @@ class AppBlockerUtil {
         store.application.denyAppRemoval = false
         
         store.clearAllSettings()
-        
-        SharedData.selection = nil
-        SharedData.strict = nil
-        SharedData.allowOnly = nil
-
-        let center = DeviceActivityCenter()
-        center.stopMonitoring([.daily])
     }
 }
