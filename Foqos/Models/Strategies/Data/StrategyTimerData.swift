eb28c1ba1a38089d1badf870177697447cde577a
Ali Waseem <waseema393@gmail.com>
2025-11-29 13:39:16 -0700

    updated timer values

--- Foqos/Models/Strategies/Data/StrategyTimerData.swift
+++ Foqos/Models/Strategies/Data/StrategyTimerData.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
 struct StrategyTimerData: Codable {
-  var durationInSeconds: Int
+  var durationInMinutes: Int
 
   static func toStrategyTimerData(from data: Data) -> StrategyTimerData {
     do {
       return try JSONDecoder().decode(StrategyTimerData.self, from: data)
     } catch {
       // If decoding fails, return a default with 15 minutes
-      return StrategyTimerData(durationInSeconds: 10 * 60)
+      return StrategyTimerData(durationInMinutes: 15)
     }
   }
 
