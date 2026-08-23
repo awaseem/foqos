f23e5c5c894a8f4d6241dacd42d716bf8d23df83
Ali Waseem <waseema393@gmail.com>
2026-08-22 19:45:47 -0600

    Remove beta tag from Temporary Access

--- Foqos/Models/Strategies/NFCSoftUnblockBlockingStrategy.swift
+++ Foqos/Models/Strategies/NFCSoftUnblockBlockingStrategy.swift
@@ -14,7 +14,6 @@ final class NFCSoftUnblockBlockingStrategy: BlockingStrategy {
   var usesNFC: Bool = true
   var startsManually: Bool = true
   var allowsTimedBreaks: Bool = false
-  var isBeta: Bool = true
   var startViewPresentationDetents: Set<PresentationDetent> = [.large]
 
   var onSessionCreation: ((SessionStatus) -> Void)?
--- Foqos/Models/Strategies/QRSoftUnblockBlockingStrategy.swift
+++ Foqos/Models/Strategies/QRSoftUnblockBlockingStrategy.swift
@@ -14,7 +14,6 @@ final class QRSoftUnblockBlockingStrategy: BlockingStrategy {
   var usesQRCode: Bool = true
   var startsManually: Bool = true
   var allowsTimedBreaks: Bool = false
-  var isBeta: Bool = true
   var startViewPresentationDetents: Set<PresentationDetent> = [.large]
 
   var onSessionCreation: ((SessionStatus) -> Void)?
